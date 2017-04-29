fbf6baf579c2e0dfd9385e86869ea25c60704d93
root <root@debian.debiqn>
2017-04-29 10:18:37 +0200

    Added note class

--- src/app/note.spec.ts
+++ src/app/note.spec.ts
@@ -1,7 +1,16 @@
 import {Note} from './note';
 
 describe('Note', () => {
-  it('should create an instance', () => {
-    expect(new Note()).toBeTruthy();
-  });
+    it('should create an instance', () => {
+        expect(new Note()).toBeTruthy();
+    });
+
+    it('should accept values in the constructor', () => {
+        let note = new Note({
+            title: 'Note Title',
+            content: 'Note Content'
+        });
+        expect(note.title).toEqual('Note Title');
+        expect(note.content).toEqual('Note Content');
+    });
 });
--- src/app/note.ts
+++ src/app/note.ts
@@ -1,2 +1,9 @@
 export class Note {
+    id: number;
+    title: string;
+    content: string;
+
+    constructor(values: Object = {}) {
+        Object.assign(this, values);
+    }
 }
